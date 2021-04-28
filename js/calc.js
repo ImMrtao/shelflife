3f25b463db4bc0b1eeffbe945dfe8cfb47202f49
ztao <immrtao@163.com>
2021-04-29 04:58:41 +0800

    - 在保质期输入框后面添加一个选择年月日的下拉框 - 修改按键0和删除键的大小

--- js/calc.js
+++ js/calc.js
@@ -4,7 +4,7 @@
  * @Author: @TAO
  * @Date: 2021-03-06 12:09:56
  * @LastEditors: sueRimn
- * @LastEditTime: 2021-04-28 13:12:44
+ * @LastEditTime: 2021-04-29 04:52:27
  */
 
 import { dateFormat, showMsg } from "../module/util.js";
@@ -12,6 +12,7 @@ import { dateFormat, showMsg } from "../module/util.js";
 //变量
 let today; //当前日期
 let shelfLife; //保质期
+let timeUnit = 1;//时间单位【年，月，日】
 let resBox;//显示计算结果
 let msgBox;//消息盒子
 let keypad;//键盘
@@ -47,6 +48,21 @@ window.addEventListener("load", function () {
 			}
 		});
 
+		//修改保质期时间的单位
+		form.on('select(time-unit)', function (data) {
+			//逻辑处理
+			const element = data.elem;
+			// 获取
+			timeUnit = data.value;
+			let msg = {
+				'1': '日',
+				'30': '月',
+				'365': '年'
+			}
+			showMsg('切换为' + msg[timeUnit], 'ok', msgBox);
+			//重新计算
+			calc();
+		});
 		//修改商品品类
 		form.on('select(category)', function (data) {
 			//逻辑处理
@@ -70,10 +86,6 @@ window.addEventListener("load", function () {
 		classNames = ["", null].includes(classNames) ? element.parentElement.getAttribute("class") : classNames;
 		if (["SPAN", "A"].includes(element.nodeName) && classNames.includes("num-btn")) {//数字按键
 			var str = String(shelfLife.value) + String(element.innerText);
-			//调用消息模块
-			// if (Number.parseInt(str) > 3652) {
-			// 	showMsg("保质期太长了", "info", msgBox);
-			// }
 			shelfLife.value = Number.parseInt(str);
 			calc();
 		}
@@ -87,22 +99,21 @@ window.addEventListener("load", function () {
 
 });
 
-// 给数字按键绑定计算方法
 /**
  * 计算保质期
  * @returns 计算结果
  */
 function calc() {
-	let timeLength = shelfLife.value; //保质期天数
+	let dayLength = shelfLife.value * timeUnit; //保质期天数
 	//无效保质期
-	if ([null, undefined, "", 0].includes(timeLength)) {
+	if ([null, undefined, "", 0].includes(dayLength)) {
 		return;
-	} else if (timeLength < 2) {
+	} else if (dayLength < 2) {
 		//showMsg('保质期应>=2', 'err', msgBox);
 		return;
 	}
 	//计算生产日期
-	let date = new Date(now.getTime() - (timeLength - category) * 86400000);
+	let date = new Date(now.getTime() - (dayLength - category) * 86400000);
 	resBox.value = dateFormat(date);
 }
 
